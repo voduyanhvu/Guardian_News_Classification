cdf906515985cde100d092904074742757e08b6f
Andrew Vo <53525943+voduyanhvu@users.noreply.github.com>
2023-09-24 10:20:01 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ chart_df = df.groupBy("Section").count().sort("Section", ascending=False)
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
 st.title("Guardian News Classifier")
-
-# Display a chart showing the number of articles section-wise
-st.bar_chart(chart_df, x='Section', use_container_width=True)
+with st.container():
+    # Display a chart showing the number of articles section-wise
+    st.bar_chart(chart_df, x='Section', use_container_width=True)
 
 # List of sections
 st.sidebar.title("Section")
@@ -47,10 +47,35 @@ pd = labels_df.toPandas()
 menu = list(pd["Section"])
 choice = st.sidebar.selectbox("Select a section", menu)
 
+# Initialize the current index
+if "current_index" not in st.session_state:
+    st.session_state.current_index = 0
+
+# this function is called when btnNext is clicked
+def handleNextClick():
+    # Show next element in list
+    st.write(st.session_state.current_index)
+    # Update and store the index
+    if st.session_state.current_index < (maxNum-1):
+       st.session_state.current_index += 1
+
+# this function is called when btnPrev is clicked
+def handlePrevClick():
+    # Show next element in list
+    st.write(st.session_state.current_index)
+    # Update and store the index
+    if st.session_state.current_index > 0:
+       st.session_state.current_index -= 1
+
 # Return the results
 if choice != "":
     result_df = df.filter(df["Section"] == choice)
-    st.table(result_df)
-
-
-
+    maxNum = result_df.count()
+    st.write("Number of articles: ", maxNum)
+    # st.table(result_df)
+    if maxNum > 0:
+        # create the button and set the on_click `callable` (aka function without ()).
+        btnNext = st.button('next', key='next', on_click=handleNextClick)
+        btnPrev = st.button('prev', key='prev', on_click=handlePrevClick)
+        txt = result_df.collect()[st.session_state.current_index][1]
+        st.write(txt)
